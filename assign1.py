f8c2aeb884cb2fe5db2ebdeb15c7b09383d9dafb
pasaremina22 <107065070+pasaremina22@users.noreply.github.com>
2022-06-08 16:12:13 +0530

    Program to find number is +ve or -ve
    Using condition statement find number is positive or negative

---
+++ assign1.py
@@ -0,0 +1,8 @@
+# program to find number is negative or positive
+num = int(input("Enter Number"))
+if num == 0:
+    print(num,"is zero")
+elif num > 0:
+    print(num,"number is positive")
+else:
+    print(num,"number is negative")
